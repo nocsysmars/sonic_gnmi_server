ccf598a0d9bb12be3247350f1ecd75e17fe56739
macauleycheng <macauley_cheng@acctonn.com>
2020-12-15 20:03:32 -0500

    fix service start fail withouth install postinst

--- build_deb.sh
+++ build_deb.sh
@@ -7,4 +7,6 @@ python setup.py --command-packages=stdeb.command debianize
 
 tar czvf ../nocsys-sonic-gnmi-server_0.1.orig.tar.gz .
 
+cp postinst debian/python-nocsys-sonic-gnmi-server.postinst
+
 dpkg-buildpackage
